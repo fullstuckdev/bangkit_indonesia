a3b124e0757abda07c2d79d1a0d76feacd9ccf07
fullstuckdev <informatikataufik@gmail.com>
2022-12-06 22:39:59 +0700

    Simplikasi

--- admin/_config.php
+++ admin/_config.php
@@ -1,6 +1,18 @@
 <?php
-$con = mysqli_connect('localhost', 'root', '', 'indopmm');
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "indopmm";
+
+$con = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 if (!$con) {
     echo 'Gagal terhubung ke database';
     die;
+}
+
+try {    
+    $db = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+} catch(PDOException $e) {
+    //show error
+    die("Terjadi masalah: " . $e->getMessage());
 }
--- admin/login.php
+++ admin/login.php
@@ -1,16 +1,6 @@
 <?php 
 
-$db_host = "localhost";
-$db_user = "root";
-$db_pass = "";
-$db_name = "indopmm";
-
-try {    
-    $db = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
-} catch(PDOException $e) {
-    //show error
-    die("Terjadi masalah: " . $e->getMessage());
-}
+include '_config.php';
 
 if(isset($_POST['login'])){
 
