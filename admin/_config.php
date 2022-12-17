759be62eb12d7648040ff0c8c154d40cb4acc1c4
fullstuckdev <informatikataufik@gmail.com>
2022-12-17 21:22:54 +0700

    update database

--- admin/_config.php
+++ admin/_config.php
@@ -2,7 +2,7 @@
 $db_host = "localhost";
 $db_user = "root";
 $db_pass = "";
-$db_name = "indopmm";
+$db_name = "bangkit_indonesia";
 
 $con = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 if (!$con) {
